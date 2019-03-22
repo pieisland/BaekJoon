fbb98178897a0480ec82edc7981db94330cd3a56
DESKTOP-35R8C3Q\pieis <pieisland@naver.com>
2019-03-23 00:06:37 +0900

    대분수. 복잡도가 높음

--- py/step8/1193.py
+++ py/step8/1193.py
@@ -3,16 +3,23 @@
 <대분수>
 '''
 
-n=int(input())
-i=1
-sum=0
-while(1):
-    sum=(i*(i+1))//2
-    if sum>=n:
-        k=0
-        while(1):
-            if sum-k==n:
-                print("%d/%d"%(k+1, i-k))
-                break
-            k+=1
+def main(): 
+    n=int(input())
+    i=1
+    sum=0
+    b=True
+    while(1):
+        sum=(i*(i+1))//2
+        if sum>=n:
+            k=0
+            while(1):
+                if sum-k==n:
+                    if i%2==0:
+                        print("%d/%d"%(i-k, k+1))
+                    else:
+                        print("%d/%d"%(k+1, i-k))
+                    return
+                k+=1
+        i+=1
 
+main()
