04047909bdb2b731c28541d23b76aecb34183376
DESKTOP-35R8C3Q\pieis <pieisland@naver.com>
2019-03-26 23:57:35 +0900

    아니 왜 틀린거지...

--- py/step12/11866.py
+++ py/step12/11866.py
@@ -9,17 +9,21 @@ for i in range(1, n+1):
     li.append(i)
 result=[]
 cnt=0
-
+i=0
 while(len(result)<n):
-    cnt+=1
-    if cnt>n:
-        cnt=1
-    if cnt%m==0:
-        if li[cnt]==-1:
-            cnt-=1
-            continue
-        li[cnt]=-1
-        result.append(li[cnt])
-        print(li[cnt])
+    i+=1
+    if i>n: i=1
+    if li[i]!=-1:
+        cnt+=1
+        if cnt%m==0:
+            result.append(li[i])
+            li[i]=-1
+            cnt=0
+for i in range(n):
+    if i==0:
+        print("<%d, "%result[i], end="")
+    elif i==n-1:
+        print("%d>"%result[i])
+    else:
+        print("%d, "%result[i], end="")
 
-#헷갈림;;
