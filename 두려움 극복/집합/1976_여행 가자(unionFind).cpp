04e3d080f49b93d7eef6aea8def133f476d587b9
pieisland <35261724+pieisland@users.noreply.github.com>
2020-10-23 12:39:34 +0900

    Create 1976_여행 가자(unionFind).cpp

---
+++ 두려움 극복/집합/1976_여행 가자(unionFind).cpp
@@ -0,0 +1,69 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int n, m;
+int cities[201][201]; //도시의 연결관계 저장.
+int parent[201];
+
+//최상위 부모를 찾아 return.
+int findParent(int n) {
+	if (parent[n] == n) return n;
+	return parent[n] = findParent(parent[n]);
+}
+
+//부모 연결..
+void unionSet(int a, int b) {
+	a = findParent(a);
+	b = findParent(b);
+
+	//크기를 비교하지 않는 이유는
+	//저기서 중복을 막아서인가?
+	parent[b] = a;
+}
+
+int main(void){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+	cin >> m;
+
+	for (int i = 1; i <= n; i++) parent[i] = i;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> cities[i][j];
+			if (cities[i][j] == 1) {
+				//연결이 돼있다면 union해준다.
+				if(findParent(i) != findParent(j))
+					unionSet(i, j);
+			}
+		}
+	}
+
+	int firstCity;
+	cin >> firstCity;
+	firstCity = findParent(firstCity);
+	
+	//같은 부모를 가지면 같은 집합에 속한 거라 갈 수 있는 길이 있다.
+	string answer = "YES";
+	for (int i = 1; i < m; i++) {
+		int x;
+		cin >> x;
+		if (findParent(x) != firstCity) {
+			answer = "NO";
+			break;
+		}
+	}
+
+	cout << answer << '\n';
+
+	return 0;
+}
