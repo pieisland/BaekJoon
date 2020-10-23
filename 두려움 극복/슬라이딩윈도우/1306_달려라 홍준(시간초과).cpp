b59c88d6a1456c4c157fb70d39c6920a86b653b9
pieisland <35261724+pieisland@users.noreply.github.com>
2020-10-24 00:00:06 +0900

    Update and rename 1306_달려라 홍준(틀림..).cpp to 1306_달려라 홍준(시간초과).cpp

--- 두려움 극복/슬라이딩윈도우/1306_달려라 홍준(시간초과).cpp
+++ 두려움 극복/슬라이딩윈도우/1306_달려라 홍준(시간초과).cpp
@@ -21,6 +21,14 @@ int findBrightest(int idx) {
 	}
 }
 
+void show() {
+	cout << "개수" << endl;
+	for (int i = 1; i <= 5; i++) {
+		cout << lightCnt[i] << " ";
+	}
+	cout << endl;
+}
+
 int main(void){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
@@ -29,26 +37,36 @@ int main(void){
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) { 
 		cin >> light[i]; 
-		lightCnt[light[i]] += 1;
 	}
 
 	int brightest = 0;
 	for (int i = 0; i < 2*m - 1; i++) {
 		int val = light[i];
+		lightCnt[light[i]] += 1;
+
 		if (val > brightest)
 			brightest = val;
 	}
+
 	cout << brightest << ' ';
 
 	int e = 0, f = 2 * m-1;
 	while (f < n) {
 		int l1 = light[e];
 		int l2 = light[f];
+		//cout << "l1, l2: " << l1 << " " << l2 << endl;
 		lightCnt[l1] -= 1;
 		lightCnt[l2] += 1;
-		if (l2 > brightest) brightest = l2;
+
+		//show();
+
+		if (l2 > brightest) { 
+			brightest = l2; 
+		  // cout << "더 큼" << endl;
+		}
 		else {
 			if (lightCnt[brightest] == 0) {
+				//cout << "없어져서 변경" << endl;
 				brightest = findBrightest(brightest);
 			}
 		}
